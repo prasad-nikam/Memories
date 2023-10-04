231760e1cd65fe9dda9328faf5983586ea5b0b68
Prasad Nikam <nikamprasad52@gmail.com>
2023-10-04 13:35:45 +0530

    database connected and routing folder structure created

--- server/index.js
+++ server/index.js
@@ -3,27 +3,25 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+import router from './routes/posts.js';
+
 const app=express();
-// Import the Express.js library
 
-// Define a route for the root URL ("/")
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
-});
+app.use('/posts',router);
 
-// Define a route for "/about"
-app.get('/about', (req, res) => {
-  res.send('This is the About page.');
-});
+app.use(bodyParser.json({limit:"30mb",extended: true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended: true}));
+app.use(cors());
 
-// Define a route for handling 404 errors
-app.use((req, res) => {
-  res.status(404).send('404 - Not Found');
-});
+const CONNECTION_URL = 'mongodb+srv://memories:memosa123@cluster0.xqconyq.mongodb.net/?retryWrites=true&w=majority';
 
-// Start the server on port 3000
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const PORT = process.env.PORT | 5000;
 
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+    .then(()=> app.listen(PORT, ()=>console.log(`Server running on port http://localhost:${PORT}`)))
+    .catch((error)=>console.log("fail to connect to MongoDB: "+error.message));
+
+app.get('/',(req,res)=>{
+    res.send('prasad');
+})
+// mongoose.set('useFindAndModify',false);
